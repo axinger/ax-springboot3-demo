3ea5b418ba58ad581d5eea1a7dc34941f907d225
axinger <liu_weixing@qq.com>
2023-12-01 11:10:30 +0800

    update

--- springboot3-mvc/src/test/java/com/axing/demo/MvcApplicationTest.java
+++ springboot3-mvc/src/test/java/com/axing/demo/MvcApplicationTest.java
@@ -3,7 +3,10 @@ package com.axing.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @SpringBootTest
 class MvcApplicationTest {
@@ -21,6 +24,15 @@ class MvcApplicationTest {
                 .uri("say1/{content}", "hello")
                 .retrieve()
                 .body(String.class);
+
+        String res2 = restClient
+                .post()
+                .uri("say1/{content}", "hello")
+                .body(Map.of())
+                .retrieve()
+                .body(String.class)
+        ;
+
 
         WebClient.builder()
                 .baseUrl("")
